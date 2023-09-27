f204fa864109ebe7d62fed030c9d82bcc054194f
Karl Keller <kgk86@hotmail.com>
2023-09-27 20:46:15 +0200

    Add React Router and render <BrowserRouter> in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import PostsList from './components/PostsList';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <div>
         <h1>React on Rails Blog</h1>
         <p>Find this application layout in client/src/App.jsx</p>
         <PostsList />
       </div>
-    </>
+    </Router>
   )
 }
 
